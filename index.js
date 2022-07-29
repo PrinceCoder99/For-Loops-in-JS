e69857d87f368395d00d08bb9bd38d47b102d999
PrinceCoder99 <108682837+PrinceCoder99@users.noreply.github.com>
2022-07-29 11:09:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// Chapter - 3 [Loops]
-
-// For Loops
-/*
-let sum = 0
-let n = prompt('Enter a value of n')
-n = Number.parseInt(n)
-for(let i = 0; i<n; i++){
-  sum += (i+1)
-}
-  console.log("Sum of first "+ n + " natural number is " + sum)
-*/
-
-
-// For in Loops
-/*
-let obj = {
-  Prince: 89,
-  Nitish: 88,
-  Niraj: 87,
-  Uchit: 86,
-}
-for(let a in obj){
-  console.log("Number of " + a + " is " + obj[a] )
-}
-*/
-
-// For of Loops
-/*
-for(let b of "Prince"){
-  console.log(b)
-}
-*/
-
-
 // Quick Quiz of For Loops
 // ------------Factorial Number--------------
 let n = prompt("Enter a number")
@@ -41,4 +6,3 @@ for(i = 0; i<n; i++){
   fact *= (i+1)
 }
 console.log("Factorial of " + n + " number is "+ fact)
-
